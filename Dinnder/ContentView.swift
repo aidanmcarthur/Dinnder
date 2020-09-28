a3b91c872bcf422c72e319dfeaa7d172e2950ffe
aidanmcarthur <tessa8934@gmail.com>
2020-09-28 14:32:37 -0700

    Initial Commit

---
+++ Dinnder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dinnder
+//
+//  Created by Aidan McArthur on 9/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dinnder/DinnderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinnderApp.swift
+//  Dinnder
+//
+//  Created by Aidan McArthur on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct DinnderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
